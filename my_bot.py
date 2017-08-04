1b1649e9b35f8f3858a1557e2cd8ca998c7c61e5
gopjak36 <gop.jak.36@gmail.com>
2017-08-04 14:00:55 +0300

    add Get Chat id and Send Message (method)

--- my_bot.py
+++ my_bot.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://api.telegram.org/bot...token..."     # Bot url woth bot token
+url = "https://api.telegram.org/bot...token..."     # Bot url with bot token
 
 ''' Get All Updates JSON format '''
 def get_updates_json(reqeust):
@@ -12,3 +12,17 @@ def last_update(data):
     results = data['result']
     last_update = len(results) - 1
     return results[last_update]
+
+''' Get Chat id from Last Update '''
+def get_chat_id(update):
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+''' Send Message '''
+def send_mess(chat, text):
+    param = {'chat_id':chat,'text':text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+chat_id = get_chat_id(last_update(get_updates_json(url))) # Chat id
+send_mess = send_mess(chat_id, 'Message text here!') # Send Message            
