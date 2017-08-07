136a35522753ec7ab9438ffde38b5bce6870a258
gopjak36 <gop.jak.36@gmail.com>
2017-08-07 09:48:48 +0300

    Encapsulate all function in one class

--- my_bot.py
+++ my_bot.py
@@ -1,39 +1,44 @@
+# -*- coding: utf-8 -*-
 import requests
-from time import sleep
+import datetime
 
-url = "https://api.telegram.org/bot371331492:AAG0_Ddqkvw-TJr8TxRLig6fNt8mZ1bLXmY//"     # Bot url with bot token
+class Bot:
+    ''' Bot class '''
 
-''' Get All Updates JSON format '''
-def get_updates_json(request):
-    params = {'timeout': 100, 'offset': None}
-    response = requests.get(request + 'getUpdates', data=params)
-    return response.json()
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}".format(token)
 
-''' Get Latest Update '''
-def last_update(data):
-    results = data['result']
-    last_update = len(results) - 1
-    return results[last_update]
+    def get_updates(self, offset=None, timeout=30):
+        ''' Get All Updates JSON format '''
+        method = 'getUpdates'
+        params = {'timeout':timeout, 'offset':offset}
+        response = reqeusts.get( self.api_url + method, params)
+        result_json = response.json()['result']
+        return result_json
 
-''' Get Chat id from Last Update '''
-def get_chat_id(update):
-    chat_id = update['message']['chat']['id']
-    return chat_id
+    def get_last_update(self):
+        ''' Get Latest Update '''
+        get_result = self.get_updates()
 
-''' Send Message '''
-def send_mess(chat, text):
-    params = {'chat_id':chat,'text':text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
 
-''' Main method using short polling '''
+        return last_update
+
+    def send_message(self, chat_id, text):
+        ''' Send Message '''
+        method = 'sendMessage'
+        params = {'chat_id':chat_id, 'text':text}
+        response = requests.post( self.api_url + method, params)
+        return response
+
+
+''' Main method using long polling '''
 def main():
-    update_id = last_update(get_updates_json(url))['update_id']
-    while True:
-        if update_id == last_update(get_updates_json(url))['update_id']:
-           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
-           update_id += 1
-    sleep(1)
+    pass
 
 if __name__ == '__main__':
     main()
