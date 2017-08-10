72155c89d96f5b3ca5a2a283fc1ec0018333205c
gopjak36 <gop.jak.36@gmail.com>
2017-08-10 23:18:45 +0300

    update Bot Greeting you (depending on the time)

--- my_bot.py
+++ my_bot.py
@@ -35,11 +35,16 @@ class Bot:
         response = requests.post( self.api_url + method, params)
         return response
 
-bot = Bot("your_token_here")
+''' Help Variables '''
+bot = Bot('371331492:AAG0_Ddqkvw-TJr8TxRLig6fNt8mZ1bLXmY')
+greetings = (u'здравствуй', u'привет', u'ку', u'здорово')
+now = datetime.datetime.now()
 
-
+''' Main Function '''
 def main():
     new_offset = None
+    today = now.day
+    hour = now.hour
 
     while True:
         bot.get_updates(new_offset)
@@ -47,12 +52,26 @@ def main():
         last_update = bot.get_last_update()
 
         last_update_id = last_update['update_id']
-        last_chat_id = last_update['message']['chat']['id']
         last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
 
-        bot.send_message(last_chat_id, last_chat_text)
+        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            bot.send_message(last_chat_id, u'Доброе утро, {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            bot.send_message(last_chat_id, u'Добрый день, {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            bot.send_message(last_chat_id, u'Добрый вечер, {}'.format(last_chat_name))
+            today += 1
+
         new_offset = last_update_id + 1
 
-
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
